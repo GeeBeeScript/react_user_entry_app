50560947e362a1a55390cbb36d8d57d7f9420dd0
GeeBeeScript <gbemigashodimu@gmail.com>
2025-02-10 23:50:30 +0100

    remove console statements

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Entry from "./components/Entry/Entry";
 function App() {
   const initialValue = { name: "Jess", age: "21", id: "0.1" };
   const [allUsers, setAllUsers] = useState([initialValue]);
-  console.log(initialValue, allUsers);
 
   const liftFurtherHandler = (user) => {
     setAllUsers((prevState) => {
@@ -18,10 +17,8 @@ function App() {
   
   return (
     <div className="App">
-      {/* {console.log("check: " + Boolean(errorStatement.isError))} */}
       <NewEntry liftFurther={liftFurtherHandler} />
       <Entry users={allUsers} />
-      {/* {console.log("check: " + errorStatement + errorStatement.isError)} */}
     </div>
   );
 }
